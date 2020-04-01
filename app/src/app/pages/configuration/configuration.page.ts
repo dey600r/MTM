a1c40131c80ab98bfefe98d431516d38f779fd0e
dey600r <dey125r@gmail.com>
2020-04-01 19:27:50 +0200

    Reorganice item list of moto, operation and configuration

--- app/src/app/pages/configuration/configuration.page.ts
+++ app/src/app/pages/configuration/configuration.page.ts
@@ -95,4 +95,55 @@ export class ConfigurationPage implements OnInit {
 
   }
 
+  getIconReplacement(maintenanceElement: MaintenanceElementModel): string {
+    switch (maintenanceElement.id) {
+      case 1:
+      case 2:
+      case 22:
+      case 23:
+        return 'disc';
+      case 4:
+      case 5:
+        return 'basket';
+      case 6:
+        return 'flash';
+      case 9:
+      case 14:
+      case 15:
+        return 'thermometer';
+      case 3:
+      case 7:
+      case 8:
+      case 12:
+        return 'color-fill';
+      case 11:
+      case 13:
+      case 16:
+        return 'layers';
+      case 18:
+      case 19:
+        return 'eyedrop';
+      case 10:
+      case 17:
+      case 20:
+      case 21:
+        return 'settings';
+      default:
+        return this.getRandomIcon();
+    }
+
+  }
+
+  getRandomIcon(): string {
+    switch (Math.floor(Math.random() * (3 - 1) + 1)) {
+      case 1:
+        return 'bulb';
+      case 2:
+        return 'bandage';
+      case 3:
+        return 'briefcase';
+      default:
+        return 'barbell';
+    }
+  }
 }
