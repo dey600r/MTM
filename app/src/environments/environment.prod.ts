77e11de1347d28cb05fd8287cb3970074b1e460f
dey600r <dey125r@gmail.com>
2022-01-07 01:21:53 +0100

    Deploy v3.3.0

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   lastVersion: 'v3.3.0',
-  lastUpdate: '2022-01-01 19:00:00',
+  lastUpdate: '2022-01-08 19:00:00',
   isFree: false,
   pathTranslate: './assets/i18n/'
 };
