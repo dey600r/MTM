2d71efd8cb09ab19ccd149ad39c4b2e78242aa3c
dey125r <dey125r@gmail.com>
2022-10-16 17:48:17 +0200

    Configure Jenkins Karma config

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -42,15 +42,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    //browsers: ['Chrome'],
     //browsers: ['ChromeHeadless'],
-    //browsers: ['ChromeHeadlessNoSandbox'],
-    // customLaunchers: {
-    //   ChromeHeadlessNoSandbox: {
-    //     base: 'ChromeHeadless',
-    //     flags: ['--no-sandbox']
-    //   }
-    // },
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     //singleRun: false,
   });
 };
