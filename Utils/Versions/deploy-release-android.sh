e06b18804df792f540eb430c956cd91e3bf88eb4
dey125r <dey125r@gmail.com>
2022-11-08 19:47:01 +0100

    Fix paths to deploy

--- Utils/Versions/deploy-release-android.sh
+++ Utils/Versions/deploy-release-android.sh
@@ -10,5 +10,5 @@ cd $path/app;
 rm -f -r $path/Utils/Versions/Android/MtM$free$version;
 mkdir $path/Utils/Versions/Android/MtM$free$version;
 ionic cordova build android --release --configuration=$prod && \
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass "$pass" -keystore $path/Utils/Versions/Android/mtm-release-prod-key.keystore $path/App/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $alias && \
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass "$pass" -keystore $path/Utils/Versions/Android/mtm-release-prod-key.keystore $path/app/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $alias && \
 zipalign -v -f 4 $path/app/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $path/Utils/Versions/Android/MtM$free$version/MtM$free$version.apk;
