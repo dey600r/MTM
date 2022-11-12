fd4fc29a396a5b6797a68836bd49a6ce891c5789
dey125r <dey125r@gmail.com>
2022-11-12 10:25:20 +0100

    Fix config

--- app/mtm-deploy-store.sh
+++ app/mtm-deploy-store.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 path=$1
 version=$2
-echo "---> START BUILD MTM $version on $path <----"
+echo "----> START BUILD MTM $version on $path <----"
 cd $path/Utils/Versions/;
 sh deploy-release-android.sh $path _ $version production $PASS_MTM $ALIAS_MTM &&
 sh deploy-release-android-bundle.sh $path _ $version $PASS_MTM $ALIAS_MTM &&
