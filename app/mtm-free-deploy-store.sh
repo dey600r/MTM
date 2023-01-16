f5474307374f96b196ccb31eccc7aefa8fd17d54
dey125r <dey125r@gmail.com>
2023-01-16 19:47:07 +0100

    Fix deploy

--- app/mtm-free-deploy-store.sh
+++ app/mtm-free-deploy-store.sh
@@ -4,6 +4,5 @@ version=$2
 echo "----> START BUILD MTM-FREE $version on $path <----"
 cd $path/Utils/Versions/;
 sh deploy-release-android.sh $path $version production-free $PASS_MTM $ALIAS_MTM &&
-sh deploy-release-android-bundle.sh $path $version production-free $PASS_MTM $ALIAS_MTM &&
 sh deploy-release-windows.sh $path $version production-free;
 echo "----> END BUILD MTM-FREE <----"
