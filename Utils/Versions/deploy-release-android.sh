cb75deb2020636c66088a87773f910e5a13c075f
dey125r <dey125r@gmail.com>
2024-12-29 11:42:17 +0100

    fix command line nativelibs continue error

--- Utils/Versions/deploy-release-android.sh
+++ Utils/Versions/deploy-release-android.sh
@@ -16,7 +16,7 @@ path_key=$path/Utils/Versions/Android
 path_build=$path/app/platforms/android/app/build
 path_output=$path_build/outputs
 path_release=$path_output/bundle/release
-path_native_lib=$path_build/intermediates/merged_native_libs/release/out/lib
+path_native_lib=$path_build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib
 path_retrace_r8=$path_output/mapping/release
 path_trace_store=$path_version/trace-store
 cd $path/app;
@@ -27,15 +27,20 @@ echo "--- SIGNING BUNDLE ANDROID ---"
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass "$pass" -keystore $path_key/mtm-release-prod-key.keystore $path_release/app-release.aab $alias && \
 echo "--- ZIPPING BUNDLE ANDROID ---"
 zipalign -v -f 4 $path_release/app-release.aab $path_version/MtM$free$version.aab;
-echo "--- SAVING BUNDLE NATIVE LIBS ---"
-cd $path_native_lib
+echo "--- CREATING OUTPUT NATIVE LIBS ANDROID ---"
 mkdir $path_trace_store
-zip -rv native-libs-symbols-all.zip *
-mv native-libs-symbols-all.zip $path_trace_store
-rm -rf $path_native_lib/armeabi
-zip -rv native-libs-symbols.zip *
-mv native-libs-symbols.zip $path_trace_store
 echo "--- SAVING BUNDLE RETRACE R8 ---"
 cp $path_retrace_r8/* $path_trace_store
 rm -rf $path_output
+echo "--- SAVING BUNDLE NATIVE LIBS ---"
+{ # TRY
+  cd $path_native_lib &&
+  zip -rv native-libs-symbols-all.zip * &&
+  mv native-libs-symbols-all.zip $path_trace_store &&
+  rm -rf $path_native_lib/armeabi &&
+  zip -rv native-libs-symbols.zip * &&
+  mv native-libs-symbols.zip $path_trace_store
+} || { # CATCH
+  echo "ERROR: NO NATIVE LIBS FOUND"
+}
 echo "----> END ANDROID $prod <----";
